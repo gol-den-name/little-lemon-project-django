9e5a52d0ec44451359dd3773455db597734d51e6
rochan <rochan1999@gmail.com>
2026-03-22 19:37:09 +0530

    commit init : added extra files like views and pipenv url views etc with all dependencies until actual mod of the app...__not added SU admin yet--

---
+++ restaurant/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class booking(models.Model):
+    first_name = models.CharField(max_length=200)
+    reservation_date = models.DateField()
+    reservation_slot = models.SmallIntegerField(default=10)
+
+    def __str__(self):
+        return self.first_name
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+
+    def __str__(self):
+        return self.name
