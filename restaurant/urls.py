7ee80fd8f989b02f4135eb7842d39f66b4cd37de
rochan <rochan1999@gmail.com>
2026-04-04 15:18:07 +0530

    rehashed the entire book, index htmls modified urls..added new views chnaged paths

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),  # http://127.0.0.1:8000/restaurant/
-    path("about/", views.about, name="about"),
-    path("book/", views.book, name="book"),
-    path("home/", views.index, name="home"),
+    path("", views.index, name="home"),  # This matches {% url 'home' %}
+    path("book/", views.book, name="book"),  # This matches {% url 'book' %}
 ]
