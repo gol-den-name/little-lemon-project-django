fe0ef9bc30e8d51412471f7f917e63a8d6ff49d4
rochan <rochan1999@gmail.com>
2026-04-04 15:42:17 +0530

    fixed admin py mistake 'b' resolved makemigrations error ...coded menu.html completed by gpt

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import booking, Menu
+from .models import Booking, Menu
 
-admin.site.register(booking)
+admin.site.register(Booking)
 admin.site.register(Menu)
